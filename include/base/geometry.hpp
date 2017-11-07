fca26c046a0d4deed36063a54bffab50648a7e93
Jordan Bieder <jordan.bieder@cea.fr>
2017-11-07 13:44:28 +0100

    Bug fix

--- include/base/geometry.hpp
+++ include/base/geometry.hpp
@@ -269,7 +269,7 @@ namespace geometry {
     return mat3d({{
         mat1[0]*mat2[0]+mat1[1]*mat2[3]+mat1[2]*mat2[6], mat1[0]*mat2[1]+mat1[1]*mat2[4]+mat1[2]*mat2[7], mat1[0]*mat2[2]+mat1[1]*mat2[5]+mat1[2]*mat2[8],
         mat1[3]*mat2[0]+mat1[4]*mat2[3]+mat1[5]*mat2[6], mat1[3]*mat2[1]+mat1[4]*mat2[4]+mat1[5]*mat2[7], mat1[3]*mat2[2]+mat1[4]*mat2[5]+mat1[5]*mat2[8],
-        mat1[6]*mat2[0]+mat1[6]*mat2[3]+mat1[8]*mat2[6], mat1[6]*mat2[1]+mat1[6]*mat2[4]+mat1[8]*mat2[7], mat1[6]*mat2[2]+mat1[6]*mat2[5]+mat1[8]*mat2[8],
+        mat1[6]*mat2[0]+mat1[7]*mat2[3]+mat1[8]*mat2[6], mat1[6]*mat2[1]+mat1[7]*mat2[4]+mat1[8]*mat2[7], mat1[6]*mat2[2]+mat1[7]*mat2[5]+mat1[8]*mat2[8],
         }});
   }
 

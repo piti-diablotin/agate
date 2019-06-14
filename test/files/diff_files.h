0a992a395950f908d366484b435f6910d72d0941
Jordan Bieder <jordan.bieder@uliege.be>
2019-06-14 12:25:20 +0200

    Increase a little tolerance for diff files

--- test/files/diff_files.h
+++ test/files/diff_files.h
@@ -1,3 +1,4 @@
+#define TOLERANCE 1e-13
 #define DIFF_FILES(fref,fnew) { \
   TS_ASSERT(fref.good()); \
   TS_ASSERT(fnew.good()); \
@@ -9,7 +10,7 @@
     double ref, value; \
     fref >> ref; \
     fnew >> value; \
-    TS_ASSERT_DELTA(ref,value,1e-14); \
+    TS_ASSERT_DELTA(ref,value,TOLERANCE); \
   } \
   TS_ASSERT(fref.eof()&&fnew.eof()); \
 }

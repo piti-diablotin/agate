836eba531e5c0376a6d893944157073d65c123e1
Jordan Bieder <jordan.bieder@uliege.be>
2019-06-18 11:39:06 +0200

    Do not check for relative error if ref is too small

--- test/files/diff_files.h
+++ test/files/diff_files.h
@@ -1,5 +1,6 @@
 #define ABSTOL 1e-6
 #define RELTOL 1e-2
+#define COMPREL 1e-10
 #define DIFF_FILES(fref,fnew) { \
   TS_ASSERT(fref.good()); \
   TS_ASSERT(fnew.good()); \
@@ -16,7 +17,7 @@
     message << "ref " << ref << " value " << value; \
     if ( std::isnan(ref) ) \
       TSM_ASSERT_IS_NAN(message.str().c_str(),value) \
-    if ( !std::isnan((ref-value)/ref) ) \
+    if ( !std::isnan((ref-value)/ref) && ref > COMPREL ) \
       TSM_ASSERT_LESS_THAN(message.str().c_str(),(ref-value)/ref,RELTOL); \
     TSM_ASSERT_DELTA(message.str().c_str(),ref,value,ABSTOL); \
   } \

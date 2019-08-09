4a0fd4d0b0a20327bca9b3ddeb53ccb357dfc501
Jordan Bieder <jordan.bieder@uliege.be>
2019-08-09 11:08:26 +0200

    Add a convertor from decimal to fraction to write better qpt

---
+++ include/base/fraction.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file include/./fraction.hpp
+ *
+ * @brief 
+ *
+ * @author Jordan Bieder <jordan.bieder@uliege.be>
+ *
+ * @copyright Copyright 2019 Jordan Bieder
+ *
+ * This file is part of AbiOut.
+ *
+ * AbiOut is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AbiOut is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AbiOut.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+
+#ifdef _WIN32
+#include "base/win32.hpp"
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include "agate.h"
+#undef HAVE_CONFIG_H
+#endif
+
+/** 
+ *
+ */
+class Fraction {
+
+  private :
+
+  protected :
+
+  public :
+
+    /**
+     * Constructor.
+     */
+    Fraction();
+
+    /**
+     * Destructor.
+     */
+    virtual ~Fraction();
+};
+
+#endif  // FRACTION_HPP

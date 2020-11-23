47fc9f158c5b9e5b21657b4f81f18be16dd463da
Louis Bastogne <louis.bastogne@uliege.be>
2020-11-23 14:07:50 +0100

    Add maxwell Boltzman statistics and test M-B and B-E are the same at high T(K)

--- test/histcustommodes_phonons.h
+++ test/histcustommodes_phonons.h
@@ -25,7 +25,7 @@ class HistCMPhonons : public CxxTest::TestSuite
       unsigned ntime = 2000;
       const double temperature = 100;
       hist.setRandomType(HistCustomModes::Normal);
-
+      hist.setStatistics(HistCustomModes::Quantum);
       hist.buildHist(qptGrid, temperature, strainBoundsIso, HistCustomModes::Ignore, ntime);
       Supercell first(hist,0);
       first.findReference(ref);
@@ -44,5 +44,60 @@ class HistCMPhonons : public CxxTest::TestSuite
       TS_ASSERT_DELTA(mean,expectedMean,1e-2);
       TS_ASSERT_DELTA(dev,expectedDev,1e-2);
     }
+
+    void testStatistic() {
+#include "PTO_DDB.hxx"
+      Dtset ref;
+      ref.readFromFile("ref_PTO_DDB");
+      Ddb* ddb = Ddb::getDdb("ref_PTO_DDB");
+      DispDB db; 
+      db.computeFromDDB(*ddb);
+      delete ddb;
+      HistCustomModes histC(ref,db);
+      HistCustomModes histQ(ref,db);
+      const geometry::vec3d& qptGrid={1, 1, 1}; 
+      std::map<HistCustomModes::StrainDistBound,double> strainBoundsIso;
+
+      unsigned ntime = 1;
+      const double temperature = 10000;
+
+      histC.setRandomType(HistCustomModes::Normal);
+      histC.setStatistics(HistCustomModes::Classical);
+      
+      histQ.setRandomType(HistCustomModes::Normal);
+      histQ.setStatistics(HistCustomModes::Quantum);
+     
+      histC.buildHist(qptGrid, temperature, strainBoundsIso, HistCustomModes::Ignore, ntime);
+      histQ.buildHist(qptGrid, temperature, strainBoundsIso, HistCustomModes::Ignore, ntime);
+     
+      Supercell first(histC,0);
+      first.findReference(ref);
+      DispDB::qptTree modes = { { {0,0,0}, { { 7, 1, 1 } } } };  // mode 7 with 1 amplitude 1 energye (we don't care here)
+      std::vector<double> projC(ntime);
+      std::vector<double> projQ(ntime);
+#pragma omp parallel for
+      for ( unsigned itime = 0 ; itime < ntime ; ++itime ) {
+        Supercell currentC(histC,itime);
+        currentC.setReference(first);
+        projC[itime] = currentC.projectOnModes(ref, db,modes,Supercell::NONE,false)[0];
+      }
+      
+      double meanC = utils::mean(projC.begin(),projC.end());
+      double devC = utils::deviation(projC.begin(),projC.end(),meanC);
+
+
+     for ( unsigned itime = 0 ; itime < ntime ; ++itime ) {
+        Supercell currentQ(histQ,itime);
+        currentQ.setReference(first);
+        projQ[itime] = currentQ.projectOnModes(ref, db,modes,Supercell::NONE,false)[0];
+      }
+      
+      double meanQ = utils::mean(projQ.begin(),projQ.end());
+      double devQ = utils::deviation(projQ.begin(),projQ.end(),meanQ);
+
+      TS_ASSERT_DELTA(devQ,devC,1e-2)
+
+     
+     }   
 
 };
